19a234fb0a7d3480110856f77f82cd27b68101e4
orierodavid <orierodavid@yahoo.com>
2026-08-13 01:14:56 +0100

    db(groups): expand cycle and closure enums to 5-10 months

---
+++ supabase/migrations/20260813000100_group_cycle_enum_expand.sql
@@ -0,0 +1,5 @@
+ALTER TYPE public.group_cycle ADD VALUE IF NOT EXISTS 'six_month';
+ALTER TYPE public.group_cycle ADD VALUE IF NOT EXISTS 'seven_month';
+ALTER TYPE public.group_cycle ADD VALUE IF NOT EXISTS 'eight_month';
+ALTER TYPE public.group_cycle ADD VALUE IF NOT EXISTS 'nine_month';
+ALTER TYPE public.group_status ADD VALUE IF NOT EXISTS 'closed';
